5f8512fc19e506a663326eb1c63c15136de9a66c
Thomas Pieters <pieters.tg@gmail.com>
2021-06-14 17:27:28 +0200

    HomeController + Login + Authentication

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager):void
+    {
+        $user = new User();
+
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setEmail('test@cocktail.com');
+        $user->setUsername('Johny Booze Legs');
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
+        $user->setDateOfBirth(new \DateTime('2000-01-01'));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}

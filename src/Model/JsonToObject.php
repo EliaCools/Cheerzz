69d15a4d2ded22e829db77fd3531d2eb60d49d83
Elia Cools <eliacools@hotmail.com>
2021-06-15 11:41:19 +0200

    json to cocktail converter model added

--- src/Model/JsonToObject.php
+++ src/Model/JsonToObject.php
@@ -2,39 +2,85 @@
 
 namespace App\Model;
 
+use App\Entity\Cocktail;
 use App\Model\CocktailApiClient;
 
 class JsonToObject
 {
 
-    private CocktailApiClient $apiClient;
+    /**
+     * @return Cocktail[]
+     */
+    public function convertToCocktails(string $fetchMultiple) :  array{
 
-    public function __construct(CocktailApiClient $apiClient)
-    {
+        $cocktails = [];
 
-        $this->apiClient= $apiClient;
+        $decoded = json_decode($fetchMultiple);
 
-    }
-
-
-    public function converToObject(){
-
-        $decoded = json_decode($this->apiClient->fetchCocktailsByFirstLetter('e'));
+        $ingredients = [];
 
 
-        foreach( $decoded as $stdObject){
+        foreach( $decoded -> drinks as $stdObject){
+
+            // @ Todo get ingredients and measurements in multi dimensional array
+        //   for($i = 0; $i<15 ; $i++){
+        //       $ingredients[] = $stdObject-> strIngredient . $i;
+        //   }
+
+            if($stdObject->strAlcoholic === 'Alcoholic' ) {
+                $stdObject -> strAlcoholic = true;
+            }
 
 
-
-
-
+        $cocktails[] = new Cocktail(
+            $stdObject -> idDrink,
+            $stdObject -> strDrink,
+            $stdObject -> strDrinkThumb,
+            $stdObject -> strCategory,
+            $stdObject -> strGlass,
+            $ingredients,
+            $stdObject-> strInstructions,
+            $stdObject-> strAlcoholic);
         }
 
-
-        return $decoded;
+        return $cocktails;
     }
 
 
+    public function convertToSingleCocktail(string $fetchSingleCocktail) :  Cocktail
+    {
+
+        $decoded = json_decode($fetchSingleCocktail);
+
+        $ingredients = [];
+
+        $cocktails = [];
+
+        foreach( $decoded -> drinks as $stdObject){
+
+        // @ Todo get ingredients and measurements in multi dimensional array
+        //   for($i = 0; $i<15 ; $i++){
+        //       $ingredients[] = $stdObject-> strIngredient . $i;
+        //   }
+
+            if($stdObject->strAlcoholic === 'Alcoholic' ) {
+                $stdObject -> strAlcoholic = true;
+            }
+
+
+        $cocktails[]  = new Cocktail(
+            $stdObject -> idDrink,
+            $stdObject -> strDrink,
+            $stdObject -> strDrinkThumb,
+            $stdObject -> strCategory,
+            $stdObject -> strGlass,
+            $ingredients,
+            $stdObject-> strInstructions,
+            $stdObject-> strAlcoholic);
+        }
+
+         return $cocktails[0];
+    }
 
 
 }

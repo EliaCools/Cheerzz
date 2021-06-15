0aedcf74381899f326a4e9b026cf4f61135b73b7
Elia Cools <eliacools@hotmail.com>
2021-06-15 22:47:42 +0200

    appointment class added

---
+++ migrations/Version20210615121927.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210615121927 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE cocktail (id INT NOT NULL, name VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, glass VARCHAR(255) NOT NULL, ingredients_and_measurements LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', instructions VARCHAR(255) NOT NULL, is_alcoholic TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE cocktail');
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -56,6 +58,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="string", length=255)
      */
     private ?string $phone;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Appointment::class, mappedBy="requestedBy")
+     */
+    private $appointments;
+
+    public function __construct()
+    {
+        $this->appointments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -185,4 +197,34 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    /**
+     * @return Collection|Appointment[]
+     */
+    public function getAppointments(): Collection
+    {
+        return $this->appointments;
+    }
+
+ // public function addAppointment(Appointment $appointment): self
+ // {
+ //     if (!$this->appointments->contains($appointment)) {
+ //         $this->appointments[] = $appointment;
+ //         $appointment->setRequestedBy($this);
+ //     }
+
+ //     return $this;
+ // }
+
+ // public function removeAppointment(Appointment $appointment): self
+ // {
+ //     if ($this->appointments->removeElement($appointment)) {
+ //         // set the owning side to null (unless already changed)
+ //         if ($appointment->getRequestedBy() === $this) {
+ //             $appointment->setRequestedBy(null);
+ //         }
+ //     }
+
+ //     return $this;
+ // }
 }

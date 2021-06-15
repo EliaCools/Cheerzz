89bb53c18f77c1022ef1fab60672fff582e71266
KaiHelsen <kai@Helsen.info>
2021-06-15 15:52:42 +0200

    built test cases for cocktail calculator and implemented basic conversions and interpretation of JSon based fractions

--- src/Model/CocktailCalculator.php
+++ src/Model/CocktailCalculator.php
@@ -1,10 +1,93 @@
 <?php
-
+declare(strict_types=1);
 
 namespace App\Model;
 
 
+use http\Exception\InvalidArgumentException;
+use JetBrains\PhpStorm\Pure;
+
 class CocktailCalculator
 {
+    private const MEASUREMENTS = ['ml', 'cl', 'l', 'oz', 'tbsp', 'tsp', 'shot'];
+    private const OUNCE_TO_ML = 29.57;
+    private const TSP_TO_ML = 4.93;
+    private const TBSP_TO_ML = 14.79;
+    private const SHOT_TO_ML = 44.36;
 
+    //<editor-fold desc="Conversion functions">
+
+    private function convertOunceToMl(float $ounces): float
+    {
+        return $ounces * self::OUNCE_TO_ML;
+    }
+
+    private function convertTeaspoonToMl(float $tsp): float
+    {
+        return $tsp * self::TSP_TO_ML;
+    }
+
+    private function convertTableSpoonToMl(float $tbsp): float
+    {
+        return $tbsp * self::TBSP_TO_ML;
+    }
+
+    private function convertShotToMl(float $shot): float
+    {
+        return $shot * self::SHOT_TO_ML;
+    }
+
+    public function fractionToFloat(int $numerator, int $denominator): float
+    {
+        if($denominator === 0)
+        {
+            throw new InvalidArgumentException('division by zero!');
+        }
+        return $numerator / $denominator;
+    }
+
+    #[Pure] public function fractionStringToFloat(string $fraction): float
+    {
+        $values = explode('/', $fraction, 3);
+        return $this->fractionToFloat((int)$values[0], (int)$values[1]);
+    }
+
+    public function calculateMeasurement(string $input): float
+    {
+        $data = explode(" ", $input);
+
+        $value = 0;
+
+        foreach ($data as $chunk)
+        {
+            if (is_numeric($chunk))
+            {
+                $value += (int)$chunk;
+                continue;
+            }
+
+            if (preg_match("/[1-9][0-9]*\/[1-9][0-9]*/", $chunk))
+            {
+                $value += $this->fractionStringToFloat($chunk);
+                continue;
+            }
+
+            if (in_array($chunk, self::MEASUREMENTS))
+            {
+                return match (strtolower($chunk))
+                {
+                    'ml' => $value,
+                    'cl' => $value * .1,
+                    'l' => $value * .001,
+                    'oz' => $this->convertOunceToMl($value),
+                    'tsp' => $this->convertTeaspoonToMl($value),
+                    'tbsp' => $this->convertTableSpoonToMl($value),
+                    'shot' => $this->convertShotToMl($value),
+                    default => $value,
+                };
+            }
+        }
+    }
+
+    //</editor-fold>
 }
---
+++ tests/CocktailCalculatorTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests;
+use App\Model\CocktailCalculator;
+use http\Exception\InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+//to run the tests, enter the following command into the terminal:
+//./vendor/bin/phpunit
+class CocktailCalculatorTest extends TestCase
+{
+    public function testFraction(): void
+    {
+        $calculator = new CocktailCalculator();
+        self::assertEquals(.5,$calculator->fractionStringToFloat('1/2'));
+        self::assertEquals(1,$calculator->fractionStringToFloat('2/2'));
+        self::assertEquals(.25,$calculator->fractionStringToFloat('1/4'));
+        self::assertEquals(2,$calculator->fractionStringToFloat('4/2'));
+
+        self::assertEquals(0,$calculator->fractionStringToFloat('0/4'));
+    }
+
+    public function testMeasurementCalculation(): void
+    {
+        $calculator = new CocktailCalculator();
+        self::assertEquals(1, $calculator->calculateMeasurement('1 ml'));
+        self::assertEquals(1.5, $calculator->calculateMeasurement('1 1/2 ml'));
+        self::assertEquals(2, $calculator->calculateMeasurement('1 1/2 2/4 ml'));
+
+        self::assertEquals(29.57, $calculator->calculateMeasurement('1 oz'));
+
+        self::assertEquals(14.79, $calculator->calculateMeasurement('1 tbsp'));
+        self::assertEquals(4.93, $calculator->calculateMeasurement('1 tsp'));
+        self::assertEquals(44.36, $calculator->calculateMeasurement('1 shot'));
+    }
+}

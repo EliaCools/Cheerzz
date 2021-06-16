f8277128f5d17f2d79c11f5e4442cd4db9bbe7c3
KaiHelsen <kai@Helsen.info>
2021-06-16 14:15:08 +0200

    cleaned up migrations

--- migrations/Version20210616103301.php
+++ migrations/Version20210616103301.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210616094114 extends AbstractMigration
+final class Version20210616103301 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,30 +20,40 @@ final class Version20210616094114 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE bartender (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, is_verified TINYINT(1) NOT NULL, phone VARCHAR(255) NOT NULL, schedule LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', UNIQUE INDEX UNIQ_ED077FE7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE cocktail (id INT NOT NULL, name VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, glass VARCHAR(255) NOT NULL, ingredients_and_measurements LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', instructions VARCHAR(255) NOT NULL, is_alcoholic TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ingredient (id INT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, is_alcoholic TINYINT(1) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, customer_id INT NOT NULL, date_purshased DATETIME DEFAULT NULL, INDEX IDX_F52993989395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE order_line (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, purchase_order_id INT NOT NULL, quantity INT NOT NULL, price DOUBLE PRECISION NOT NULL, INDEX IDX_9CE58EE14584665A (product_id), INDEX IDX_9CE58EE1A45D7E6A (purchase_order_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, quantity DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE shopping_cart (id INT AUTO_INCREMENT NOT NULL, customer_id INT NOT NULL, name VARCHAR(255) DEFAULT NULL, INDEX IDX_72AAD4F69395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE shopping_line (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, shopping_cart_id INT NOT NULL, quantity INT NOT NULL, INDEX IDX_A81DE8B74584665A (product_id), INDEX IDX_A81DE8B745F80CD (shopping_cart_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, is_verified TINYINT(1) NOT NULL, phone VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F52993989395C3F3 FOREIGN KEY (customer_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE order_line ADD CONSTRAINT FK_9CE58EE14584665A FOREIGN KEY (product_id) REFERENCES product (id)');
         $this->addSql('ALTER TABLE order_line ADD CONSTRAINT FK_9CE58EE1A45D7E6A FOREIGN KEY (purchase_order_id) REFERENCES `order` (id)');
         $this->addSql('ALTER TABLE shopping_cart ADD CONSTRAINT FK_72AAD4F69395C3F3 FOREIGN KEY (customer_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE shopping_line ADD CONSTRAINT FK_A81DE8B74584665A FOREIGN KEY (product_id) REFERENCES product (id)');
         $this->addSql('ALTER TABLE shopping_line ADD CONSTRAINT FK_A81DE8B745F80CD FOREIGN KEY (shopping_cart_id) REFERENCES shopping_cart (id)');
-        $this->addSql('DROP TABLE appointment');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE order_line DROP FOREIGN KEY FK_9CE58EE1A45D7E6A');
+        $this->addSql('ALTER TABLE order_line DROP FOREIGN KEY FK_9CE58EE14584665A');
+        $this->addSql('ALTER TABLE shopping_line DROP FOREIGN KEY FK_A81DE8B74584665A');
         $this->addSql('ALTER TABLE shopping_line DROP FOREIGN KEY FK_A81DE8B745F80CD');
-        $this->addSql('CREATE TABLE appointment (id INT AUTO_INCREMENT NOT NULL, requested_by_id INT NOT NULL, assigned_to_id INT DEFAULT NULL, start_time DATETIME NOT NULL, end_time DATETIME NOT NULL, INDEX IDX_FE38F8444DA1E751 (requested_by_id), INDEX IDX_FE38F844F4BD7827 (assigned_to_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE appointment ADD CONSTRAINT FK_FE38F8444DA1E751 FOREIGN KEY (requested_by_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('ALTER TABLE appointment ADD CONSTRAINT FK_FE38F844F4BD7827 FOREIGN KEY (assigned_to_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F52993989395C3F3');
+        $this->addSql('ALTER TABLE shopping_cart DROP FOREIGN KEY FK_72AAD4F69395C3F3');
+        $this->addSql('DROP TABLE bartender');
+        $this->addSql('DROP TABLE cocktail');
+        $this->addSql('DROP TABLE ingredient');
         $this->addSql('DROP TABLE `order`');
         $this->addSql('DROP TABLE order_line');
+        $this->addSql('DROP TABLE product');
         $this->addSql('DROP TABLE shopping_cart');
         $this->addSql('DROP TABLE shopping_line');
+        $this->addSql('DROP TABLE user');
     }
 }

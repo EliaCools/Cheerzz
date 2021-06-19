7d686c67b7a4de27844117a478610ddf20b61c92
Elia Cools <eliacools@hotmail.com>
2021-06-19 21:06:24 +0200

    retrieve tutorial from youtube api

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\YoutubeApiClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,9 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $youtubeApi = new YoutubeApiClient($this->getParameter('app.youtube_api_key'));
+        $test = $youtubeApi->getTitle('blue margarita');
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
--- src/Model/YoutubeApiClient.php
+++ src/Model/YoutubeApiClient.php
@@ -6,55 +6,78 @@ namespace App\Model;
 
 use Google_Client;
 use Google_Service_YouTube;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class YoutubeApiClient
 {
 
-    public function fetchTrainingVideo()
+    private string $apikey;
+
+    public function __construct(string $apikey)
     {
 
+        $this->apikey = $apikey;
+    }
+
+    private function apirequest(string $cocktailName){
         /**
          * Sample PHP code for youtube.search.list
          * See instructions for running these code samples locally:
          * https://developers.google.com/explorer-help/guides/code_samples#php
          */
 
+   //    if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+   //        throw new Exception(sprintf('Please run "composer require google/apiclient:~2.0" in "%s"', __DIR__));
+   //    }
+   //    require_once __DIR__ . '/vendor/autoload.php';
+
         $client = new Google_Client();
         $client->setApplicationName('API code samples');
         $client->setScopes([
             'https://www.googleapis.com/auth/youtube.force-ssl',
         ]);
+        $client->setDeveloperKey($this->apikey);
 
-// TODO: For this request to work, you must replace
-//       "YOUR_CLIENT_SECRET_FILE.json" with a pointer to your
-//       client_secret.json file. For more information, see
-//       https://cloud.google.com/iam/docs/creating-managing-service-account-keys
-        $client->setAuthConfig('YOUR_CLIENT_SECRET_FILE.json');
-        $client->setAccessType('offline');
-
-// Request authorization from the user.
-        $authUrl = $client->createAuthUrl();
-        printf("Open this link in your browser:\n%s\n", $authUrl);
-        print('Enter verification code: ');
-        $authCode = trim(fgets(STDIN));
-
-// Exchange authorization code for an access token.
-        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-        $client->setAccessToken($accessToken);
 
 // Define service object for making API requests.
         $service = new Google_Service_YouTube($client);
 
         $queryParams = [
             'maxResults' => 1,
-            'q' => 'how to make a blue margarita cocktail'
+            'q' => 'how to make a ' . $cocktailName .' cocktail',
+            'type' => 'video',
+            'videoEmbeddable' => 'true'
         ];
 
-        $response = $service->search->listSearch('', $queryParams);
-        print_r($response);
+        $response = $service->search->listSearch('snippet', $queryParams);
+        return $response;
 
 
     }
 
+    public function getVideoUrl(string $cocktailName) : string{
+
+        $rawData = $this->apirequest($cocktailName);
+
+        return "https://www.youtube.com/watch?v=" . $rawData->getItems()[0]->getId()->getVideoId();
+
+
+    }
+    public function getTitle(string $cocktailName) : string{
+
+        $rawData = $this->apirequest($cocktailName);
+
+        return $rawData->getItems()[0]->getSnippet()->getTitle();
+
+
+    }
+    public function getDescription(string $cocktailName) : string{
+
+        $rawData = $this->apirequest($cocktailName);
+
+        return $rawData->getItems()[0]->getSnippet()->getDescription();
+
+
+    }
 
 }
